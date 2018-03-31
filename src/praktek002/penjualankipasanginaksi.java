e854ff3cf1f1313518e90fcdb280e372f60c561e
hilmanhamidi <hilmanhamidi007@gmail.com>
2018-03-31 10:37:21 +0800

    instance object

--- src/praktek002/penjualankipasanginaksi.java
+++ src/praktek002/penjualankipasanginaksi.java
@@ -3,6 +3,7 @@ package praktek002;
 
 public class penjualankipasanginaksi {
     public static void main(String[] args) {
+          penjualankipasangin r1 = new penjualankipasangin();
           
     }
 }

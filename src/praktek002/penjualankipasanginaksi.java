6ca51fa0c38d7b9829d1d2875f85e3bd4c5d76d6
hilmanhamidi <hilmanhamidi007@gmail.com>
2018-03-31 10:41:06 +0800

    mengisi nilai atribut

--- src/praktek002/penjualankipasanginaksi.java
+++ src/praktek002/penjualankipasanginaksi.java
@@ -5,5 +5,7 @@ public class penjualankipasanginaksi {
     public static void main(String[] args) {
           penjualankipasangin r1 = new penjualankipasangin();
           
+          r1.harga_kipas = 300000;
+          r1.jumlah_kipas = 2;
     }
 }
